e36e3e7ba6330db3783f663fd41bfe3b3f98737a
zmx2321 <zmx2321@163.com>
2023-12-26 14:52:10 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn build
+
+# 进入生成的文件夹
+cd dist
+
+git pull
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
+git push -f git@github.com:zmx2321/kgm-to-mp3.git master:gh-pages
+
+cd -
---
+++ main.sh
@@ -0,0 +1,5 @@
+git pull
+git add .
+git commit -m 'init'
+
+git push
